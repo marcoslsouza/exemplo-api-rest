79700153a7ae4fd9c2421664484dba2e80f94b88
Marcos L. Souza <marcoslsouzadf@gmail.com>
2021-01-13 09:54:35 -0300

    Primeiro commit

---
+++ src/main/java/com/github/marcoslsouza/exemploapirest/ExemploApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcoslsouza.exemploapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApiRestApplication.class, args);
+	}
+
+}
