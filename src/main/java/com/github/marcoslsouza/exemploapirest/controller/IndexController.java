2c7e35de48a410a35a6eb51c830da75295e06166
Marcos L. Souza <marcoslsouzadf@gmail.com>
2021-01-16 15:01:53 -0300

    Implementacao da entidade Telefone

--- src/main/java/com/github/marcoslsouza/exemploapirest/controller/IndexController.java
+++ src/main/java/com/github/marcoslsouza/exemploapirest/controller/IndexController.java
@@ -3,9 +3,11 @@ package com.github.marcoslsouza.exemploapirest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,18 +23,30 @@ public class IndexController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-
+	
 	@PostMapping(value = "/", produces = "application/json")
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public ResponseEntity<Usuario> init(@RequestBody Usuario usuario) {
-		//Usuario usuario = new Usuario();
-		//usuario.setLogin("maria.souza@gmail.com");
-		//usuario.setNome("Maria");
-		//usuario.setSenha("12355");
-		
 		usuarioRepository.save(usuario);
-		
 		return ResponseEntity.ok(usuario);
+	}
+	
+	@PutMapping(value = "/{id}", produces = "application/json")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizar(@RequestBody Usuario usuarioAtualizado, @PathVariable("id") Long id) {
+		usuarioRepository.findById(id).map(usuario -> {
+			usuarioAtualizado.setId(usuario.getId());
+			return usuarioRepository.save(usuarioAtualizado);
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/json")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void excluir(@PathVariable("id") Long id) {
+		usuarioRepository.findById(id).map(usuario -> {
+			usuarioRepository.deleteById(id);
+			return Void.TYPE;
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 	}
 	
 	@GetMapping(value = "/{id}", produces = "application/json")
---
+++ src/main/java/com/github/marcoslsouza/exemploapirest/model/Telefone.java
@@ -0,0 +1,46 @@
+package com.github.marcoslsouza.exemploapirest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Telefone {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String numero;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
